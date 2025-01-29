a2efbb3d46058e267063df84b217cd2b3cda80cf
Mr. Insignares <juansignares@gmail.com>
2025-01-29 14:51:48 -0500

    Responsive changes for text

--- src/components/global-header/index.tsx
+++ src/components/global-header/index.tsx
@@ -16,9 +16,9 @@ function Header() {
       <div className="vertical-center">
         <div className="container">
             <p className='title text'>
-                THE <span className='blue-t'>BANK</span> OF THE <span className='blue-t'>FUTURE</span>
+                THE <span className='blue-t'>BANK</span> OF<br className='d-md-none d-lg-none d-xl-none'></br> THE <span className='blue-t'>FUTURE</span>
             </p>
-            <p className='subtitle text'>Let's build the future together, one euro at a time.</p>
+            <p className='subtitle text'>Let's build the future together,<br className='d-md-none d-lg-none d-xl-none'></br> one euro at a time.</p>
         </div>
       </div>
     </>
