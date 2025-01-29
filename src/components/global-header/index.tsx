be75f1c76dda36d277238ffe79c3830306a933fd
mrinsignares <mrinsignares@MrInsignares.ht.home>
2025-01-29 13:51:58 -0500

    Background gradient and logos

--- src/components/global-header/index.tsx
+++ src/components/global-header/index.tsx
@@ -6,12 +6,20 @@ function Header() {
 
   return (
     <>
+      <div className="vertical-center">
         <div className="container">
+            <div className="row">
+              <div className="col-12 text-center">
+                <img src="/src/assets/img/ico.png" style={{paddingRight:15}} alt="" />
+                <img src="/src/assets/img/logo.png" alt="" />
+              </div>
+            </div>
             <p className='title text'>
                 THE <span className='blue-t'>BANK</span> OF THE <span className='blue-t'>FUTURE</span>
             </p>
             <p className='subtitle text'>Let's build the future together, one euro at a time.</p>
         </div>
+      </div>
     </>
   )
 }
