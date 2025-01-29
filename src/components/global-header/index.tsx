9e5703c91bbb72ef8575158d42e4ef01e9e0e1a5
mrinsignares <mrinsignares@MrInsignares.ht.home>
2025-01-29 14:16:44 -0500

    Resposive addings

--- src/components/global-header/index.tsx
+++ src/components/global-header/index.tsx
@@ -6,14 +6,14 @@ function Header() {
 
   return (
     <>
+      <div className="row pt-4 position-absolute w-100 top-0 start-0">
+        <div className="col-12 text-center">
+          <img src="/src/assets/img/ico.png" style={{paddingRight:15}} alt="" />
+          <img src="/src/assets/img/logo.png" alt="" />
+        </div>
+      </div>
       <div className="vertical-center">
         <div className="container">
-            <div className="row">
-              <div className="col-12 text-center">
-                <img src="/src/assets/img/ico.png" style={{paddingRight:15}} alt="" />
-                <img src="/src/assets/img/logo.png" alt="" />
-              </div>
-            </div>
             <p className='title text'>
                 THE <span className='blue-t'>BANK</span> OF THE <span className='blue-t'>FUTURE</span>
             </p>
